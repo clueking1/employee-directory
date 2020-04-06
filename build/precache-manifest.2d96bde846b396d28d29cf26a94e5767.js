657c49a2d54709160054f83ea3dc68c4f978dd79
clueking1 <wcoan300@gmail.com>
2020-04-06 13:14:07 -0400

    added build

---
+++ build/precache-manifest.2d96bde846b396d28d29cf26a94e5767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb98f4fc82fa899e98d5071b6cfc6fd5",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "fa4973940061a24ebf16",
+    "url": "/employee-directory/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/employee-directory/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "fa4973940061a24ebf16",
+    "url": "/employee-directory/static/js/main.e1411ac0.chunk.js"
+  },
+  {
+    "revision": "77a50e16a26474c82f8d",
+    "url": "/employee-directory/static/js/runtime~main.abed8607.js"
+  }
+]);
